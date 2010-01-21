b79502605289c973ca74fe241f88d5aa0fa3bf84
shelling <navyblueshellingford@gmail.com>
2010-01-21 16:09:33 +0800

    rename string.py to avoid conflicting

--- fdtd_string.py
+++ fdtd_string.py
@@ -3,15 +3,14 @@
 import os
 import sys
 
-
-import scipy
 import numpy
 import math
 
+fieid_array = numpy.zeros(100)
+print fieid_array
     
 class FDTD():
     def next_efield():
         return "e"
-    
     def next_hfield():
         return "h"
