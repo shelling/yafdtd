053b0a803f3dcbcaf2abdd769e4e53263cb9d960
shelling <navyblueshellingford@gmail.com>
2010-01-21 15:05:34 +0800

    initial import

---
+++ string.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+import os
+import sys
+
+
+import scipy
+import numpy
+import math
+
+    
+class FDTD():
+    def next_efield():
+        return "e"
+    
+    def next_hfield():
+        return "h"
