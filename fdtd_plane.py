3ce1432be43d0624dce01ca82b665fa1335623f3
shelling <navyblueshellingford@gmail.com>
2010-02-01 14:15:34 +0800

    put source at center

--- fdtd_plane.py
+++ fdtd_plane.py
@@ -10,13 +10,15 @@ from fdtd.algorithm.twodim.freespace import update_efield, update_hfield
 # program start
 
 new_efield = grid.Plane(shape=(301,301), timestep=1, spacestep=2*3*10**8)
+new_efield.eps[180:,0:] *= 2
 new_hfield = grid.Plane(shape=(301,301), timestep=1, spacestep=2*3*10**8)
+new_hfield.eps[180:,0:] *= 2
 
 
-for step in range(1,30):
-    new_efield.y[151,151] += float(mpmath.sin(step*mpmath.pi/6.125))*2
+for step in range(1,600):
+    new_efield.y[150,150] += float(mpmath.sin(step*mpmath.pi/6.125))*2
     new_efield = update_efield( new_efield, new_hfield )
-    new_hfield.z[151,151] += float(mpmath.sin(step*mpmath.pi/6.125))*2
+    new_hfield.z[150,150] += float(mpmath.sin(step*mpmath.pi/6.125))*2
     new_hfield = update_hfield( new_efield, new_hfield )
     
 
@@ -28,15 +30,15 @@ for step in range(1,30):
     pylab.imshow( h, interpolation="quadric", norm=matplotlib.colors.Normalize(-0.3,0.3,True) )
     pylab.colorbar()
     pylab.grid(True)
-    pylab.savefig("result/fdtd_plane-%s.png" % str(step))
+    pylab.savefig("result/fdtd_plane-%.3d.png" % step)
     pylab.clf()
 
-    d = numpy.array(new_hfield.z[0:,151], dtype="float")
-    print d[140:159]
+    d = numpy.array(new_hfield.z[0:,150], dtype="float")
+
     for i in xrange(1,len(d)-1):
         if i%2==0:
             d[i] = ( d[i-1] + d[i+1] ) / 2
-    print d[140:159]
+
     pylab.plot( d )
     pylab.ylim((-0.3,0.3))
     pylab.grid(True)
