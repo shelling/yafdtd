51ae94e57f174826b0d9ab3cb6303e065d5e244b
shelling <navyblueshellingford@gmail.com>
2010-08-05 13:50:27 +0800

    add gaussian_oft() source generator

--- lib/fdtd/source.py
+++ lib/fdtd/source.py
@@ -5,6 +5,8 @@
 #   chirp                          (not yet)
 #   polynomial                     (not yet)
 
+import math
+
 from mpmath import *
 mp.dps = 50
 
@@ -33,3 +35,28 @@ def gaussian(t, sigma, mean):
 
 def gaussian_p(t, sigma, mean):
     return ( (mean - t) / sqrt(2 * pi) * sigma**3 ) * exp( -1 * (t - mean)**2 / (2*sigma**2) )
+
+
+def gaussian_oft(t, center, width):
+    """
+    Gaussian Pulse generater
+    
+    Arguments:
+    - `center`: the center
+    - `width`:
+    """
+    return math.exp( -0.5 * math.pow((float(center) - t)/width, 2.0) )
+
+
+# triangular source
+def sin_oft(t, f=1):
+    """
+    sine of t
+    The default period (f=1) is 180 time steps to go through 2 PI.
+    Setting frequency to modify the period.
+    
+    Arguments:
+    - `t`: require
+    - `f`: default is 1
+    """
+    return math.sin(t*f*math.pi/180)
