d5ba833e8b0cb6b8f84369ddd3b337255f0f706d
shelling <navyblueshellingford@gmail.com>
2010-08-05 13:45:19 +0800

    move one dimension equations into lib

--- lib/fdtd/algorithm/onedim.py
+++ lib/fdtd/algorithm/onedim.py
@@ -1,1 +1,24 @@
 from mpmath import *
+
+
+def update_efield(efield, hfield):
+    """
+    update efield
+    
+    Arguments:
+    - `efield`:
+    """
+    for x in range(1, efield.shape[0]-1):
+        efield[x] = efield[x] + 0.5 * ( hfield[x-1] - hfield[x] )
+
+
+def update_hfield(efield, hfield):
+    """
+    update hfield
+    
+    Arguments:
+    - `efield`:
+    - `hfield`:
+    """
+    for x in range(0, hfield.shape[0]-1):
+        hfield[x] = hfield[x] + 0.5 * ( efield[x] - efield[x+1] )
--- oned-test.py
+++ oned-test.py
@@ -1,59 +1,15 @@
 #!/usr/bin/env python
 #-*- mode: python -*-
 
-import matplotlib
-matplotlib.use("Agg")                   # for linux without desktop env
-
-import pylab
-import numpy
+import convention
 import math
 import os, csv, re
 
-efield = numpy.zeros(11)
-hfield = numpy.zeros(11)
+from fdtd import source
+from fdtd.algorithm.onedim import *
 
-
-def update_efield(efield, hfield):
-    """
-    update efield
-    
-    Arguments:
-    - `efield`:
-    """
-    for x in range(1, efield.shape[0]-1):
-        efield[x] = efield[x] + 0.5 * ( hfield[x-1] - hfield[x] )
-
-
-def update_hfield(efield, hfield):
-    """
-    update hfield
-    
-    Arguments:
-    - `efield`:
-    - `hfield`:
-    """
-    for x in range(0, hfield.shape[0]-1):
-        hfield[x] = hfield[x] + 0.5 * ( efield[x] - efield[x+1] )
-
-
-def gaussian(center, width):
-    """
-    Gaussian Pulse generater
-    
-    Arguments:
-    - `center`:
-    - `width`:
-    """
-    return math.exp( -0.5 * math.pow((center - t)/width, 2.0) )
-
-def sin_oft(t,f):
-    """
-    sine of t
-    
-    Arguments:
-    - `t`:
-    """
-    return math.sin(t*f*math.pi/180)
+efield = numpy.zeros(31)
+hfield = numpy.zeros(31)
 
 
 efield_lm1 = 0
@@ -75,7 +31,7 @@ for t in range(1,140):
 
     
     xcenter = efield.shape[0]/2
-    efield[xcenter] = sin_oft(t,10)
+    efield[xcenter] = source.gaussian_oft(t, 20, 5)
 
     
     update_hfield(efield, hfield)
