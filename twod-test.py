922f530b79d864e8b7ecbf8237da934ffa1e0c52
shelling <navyblueshellingford@gmail.com>
2010-08-18 19:38:47 +0800

    start to develope UPML

--- twod-test.py
+++ twod-test.py
@@ -4,18 +4,18 @@ import convention
 import os, csv, re, gc
 
 from fdtd import source
-from fdtd.algorithm.twodim import freespace, bpml
+from fdtd.algorithm.twodim import freespace, upml
 from fdtd.grid import Plane
 from fdtd.utils import *
 
 plane = Plane( (31,31), "TM")
-bpml.append_pml( plane )
-bpml.plot_pml_params(plane, "/tmp/a.png")
+upml.append_pml( plane )
+upml.plot_pml_params(plane, "/tmp/a.png")
 
-for t in range(0,1000):
-    bpml.update_efield( plane )
+for t in range(0,100):
+    upml.update_efield( plane )
     plane.ezfield[plane.shape[0]/2, plane.shape[1]/2] = source.sin_oft(t, 10)
-    bpml.update_hfield( plane )
+    upml.update_hfield( plane )
     save_field_surf(plane.ezfield, "result/twod-testing-surface-%.3d.png", t )
     save_field(plane.ezfield, "result/twod-testing-%.3d.png", t, [-1,1])
     print t
