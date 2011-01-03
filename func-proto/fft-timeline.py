0431bf850a8628981c9558d5033e0b9ab196a249
shelling <navyblueshellingford@gmail.com>
2011-01-03 18:04:35 +0800

    take apart sample rate and number of data

--- func-proto/fft-timeline.py
+++ func-proto/fft-timeline.py
@@ -8,21 +8,28 @@ import pylab
 n = numpy.float(2**9)
 freq = 25
 
-t = numpy.arange(n)/n
+samplerate = 512
+sampletime = 1./samplerate
+
+index = numpy.arange(n)
+
+t = index*sampletime
 timedomain = numpy.cos(2*numpy.pi*freq*t)
 
-f = t*n
+f = numpy.arange(-n/2,n/2)/(sampletime*n)
 freqdomain = numpy.fft.fft(timedomain)
 
-for item in zip(f,numpy.abs(freqdomain.round())):
+for item in zip(f,numpy.fft.fftshift(numpy.abs(freqdomain).round())):
   print item
+
 
 pylab.subplot(2,1,1)
 pylab.plot(t,timedomain)
+pylab.xlim(0, n*sampletime)
 
 pylab.subplot(2,1,2)
-pylab.plot(f-n/2,numpy.fft.fftshift(numpy.abs(freqdomain)))
-pylab.xlim(-n/2,n/2)
+pylab.plot(f,numpy.fft.fftshift(numpy.abs(freqdomain)))
+# pylab.xlim(-n/2,n/2)
 
 pylab.savefig("/tmp/a.png")
 
