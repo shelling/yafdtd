05f9a62363d74af985eb1102040e6e3994cd76fb
shelling <navyblueshellingford@gmail.com>
2011-01-04 00:10:54 +0800

    fft prototype done

--- func-proto/fft-timeline.py
+++ func-proto/fft-timeline.py
@@ -5,23 +5,23 @@ import scipy
 import matplotlib
 import pylab
 
-n = numpy.float(2**9)
+DEBUG = 1
+
+n = numpy.float(2**10)
 freq = 25
 
-samplerate = 512
+samplerate = 800
 sampletime = 1./samplerate
 
-index = numpy.arange(n)
-
-t = index*sampletime
+t = numpy.arange(n)*sampletime
 timedomain = numpy.cos(2*numpy.pi*freq*t)
 
 f = numpy.arange(-n/2,n/2)/(sampletime*n)
 freqdomain = numpy.fft.fft(timedomain)
 
-for item in zip(f,numpy.fft.fftshift(numpy.abs(freqdomain).round())):
-  print item
-
+if DEBUG:
+  for item in zip(f,numpy.fft.fftshift(numpy.abs(freqdomain).round())):
+    print item
 
 pylab.subplot(2,1,1)
 pylab.plot(t,timedomain)
@@ -29,7 +29,7 @@ pylab.xlim(0, n*sampletime)
 
 pylab.subplot(2,1,2)
 pylab.plot(f,numpy.fft.fftshift(numpy.abs(freqdomain)))
-# pylab.xlim(-n/2,n/2)
+pylab.xlim(-n/2,n/2)
 
 pylab.savefig("/tmp/a.png")
 
