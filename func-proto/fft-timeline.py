05d33277f16a2fb54c27d233b30d7c0f8b57d237
shelling <navyblueshellingford@gmail.com>
2011-01-04 12:27:15 +0800

    fft of pair as a function

--- func-proto/fft-timeline.py
+++ func-proto/fft-timeline.py
@@ -5,30 +5,34 @@ import scipy
 import matplotlib
 import pylab
 
+def fft(t, timedomain, sampleperiod):
+  n = len(t)
+  f = numpy.arange(-n/2,n/2)/(sampleperiod*n)
+  freqdomain = numpy.abs(numpy.fft.fftshift(numpy.fft.fft(timedomain)))
+  return f, freqdomain
+
 DEBUG = 1
 
 n = numpy.float(2**10)
 freq = 25
 
-samplerate = 800
-sampletime = 1./samplerate
+dt = 1./800
 
-t = numpy.arange(n)*sampletime
+t = numpy.arange(n)*dt
 timedomain = numpy.cos(2*numpy.pi*freq*t)
 
-f = numpy.arange(-n/2,n/2)/(sampletime*n)
-freqdomain = numpy.fft.fft(timedomain)
+f, freqdomain = fft(t, timedomain, dt)
 
 if DEBUG:
-  for item in zip(f,numpy.fft.fftshift(numpy.abs(freqdomain).round())):
+  for item in zip(f,freqdomain.round()):
     print item
 
 pylab.subplot(2,1,1)
 pylab.plot(t,timedomain)
-pylab.xlim(0, n*sampletime)
+pylab.xlim(0, n*dt)
 
 pylab.subplot(2,1,2)
-pylab.plot(f,numpy.fft.fftshift(numpy.abs(freqdomain)))
+pylab.plot(f,freqdomain)
 pylab.xlim(-n/2,n/2)
 
 pylab.savefig("/tmp/a.png")
