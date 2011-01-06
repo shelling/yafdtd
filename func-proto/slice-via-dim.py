ac6d6cbb7769505c39ff01c6258a1f19a997af6c
shelling <navyblueshellingford@gmail.com>
2011-01-06 21:06:48 +0800

    complete algorithm of slice via dim

--- func-proto/slice-via-dim.py
+++ func-proto/slice-via-dim.py
@@ -12,20 +12,21 @@ def combination(shape):
     index = [item+[k] for item in index for k in range(z)] if z else index
     return index
 
-a = numpy.arange(0,1000)
-a.shape = (10,10,10,)
+
+
+a = numpy.arange(0,100)
+b = numpy.arange(0,1000)
+c = numpy.arange(0,10000)
+
+a.shape = (10,10,)
+b.shape = (10,10,10)
+c.shape = (10,10,10,10)
 #           t  x  y  z
 
-index = combination(a.shape)
-
-# index = [tuple(item) for item in index]
-
-print a[:,0,0]
-print index[0]
-while len(index[0]) < 3:
-    index[0].append(None)
-index[0] = tuple(index[0])
-print index[0]
-print a[:,0,0,None].flatten()
+index = combination(c.shape)
+res = c.copy()
+for i in range(len(index[0])):
+    res = res[:,index[0][i]]
+print res
 
 
