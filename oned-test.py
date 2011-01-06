73f908e9a13d6cad75c0a3ee6fb38aded87b11c1
shelling <navyblueshellingford@gmail.com>
2011-01-06 19:20:19 +0800

    set dt, dx

--- oned-test.py
+++ oned-test.py
@@ -8,25 +8,30 @@ from fdtd import source
 from fdtd.algorithm.onedim import *
 from fdtd.grid import String
 from fdtd.utils import *
+from scipy.constants import c, epsilon_0
 
-string = String(31)
-string.source = source.HardSource(source.sin_oft, (0.01,), string.shape[0]/2)
-
+dt = 10**-9
+dx = dt*c*2
+f  = dt * (10**16)
 
 name = "oned-test"
 outdir = "result/%s" % name
 prepare(outdir)
 
+string = String(31)
+string.source = source.HardSource(source.sin_oft, (f,), string.shape[0]/2)
+
 hdf5 = h5py.File("%s/%s.hdf5" % (outdir, name),"w")
-hdf5.attrs["name"] = name
+hdf5.attrs["name"]  = name
+hdf5.attrs["dt"]    = dt
+hdf5.attrs["dx"]    = dx
 hdf5.require_group("timeline")
 
-for t in range(0,200):
+for t in range(0,500):
     string.update_efield()\
           .update_abc()\
-          .update_source(t)\
-          .update_hfield()\
-          # .plot("result/oned-testing-%.3d.png", t)
+          .update_source(t*dt)\
+          .update_hfield()
     hdf5.require_group("timeline/"+str(t))
     hdf5["timeline"][str(t)]["ex"] = string.efield
     hdf5["timeline"][str(t)]["hy"] = string.hfield
