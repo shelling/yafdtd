236384d35aa8604fde94772fa2c882f6033db695
shelling <navyblueshellingford@gmail.com>
2011-01-08 15:50:25 +0800

    merge two prototype

--- func-proto/fft-timeline.py
+++ func-proto/fft-timeline.py
@@ -2,6 +2,7 @@
 
 import sys, numpy, scipy, matplotlib, pylab
 sys.path.append("./lib")
+from fdtd.source import gaussian_oft
 from fdtd.utils  import fft
 
 DEBUG = 1
@@ -20,13 +21,35 @@ if DEBUG:
   for item in zip(f,freqdomain.round()):
     print item
 
-pylab.subplot(2,1,1)
+pylab.subplot(2,2,1)
 pylab.plot(t,timedomain)
 pylab.xlim(0, n*dt)
 
-pylab.subplot(2,1,2)
+pylab.subplot(2,2,3)
 pylab.plot(f,freqdomain)
 pylab.xlim(-n/2,n/2)
+
+
+n = numpy.float(2**18)
+
+dt = 1./1024
+
+t = numpy.arange(n)*dt
+timedomain = [gaussian_oft(i, 100, 1) for i in t]
+
+f, freqdomain = fft(t, timedomain, dt)
+
+if DEBUG:
+  for item in zip(f,freqdomain.round()):
+    print item
+
+pylab.subplot(2,2,2)
+pylab.plot(t,timedomain)
+pylab.xlim(99, 101)
+
+pylab.subplot(2,2,4)
+pylab.plot(f,freqdomain)
+pylab.xlim(-2,2)
 
 pylab.savefig("/tmp/a.png")
 
