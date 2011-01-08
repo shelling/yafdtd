1385dee3c4872ce5f8dfbd5a9268ab238e378c13
shelling <navyblueshellingford@gmail.com>
2011-01-08 21:47:19 +0800

    set version as 1.0.0

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from distutils.core import setup
 
 setup(
   name            = "yafdtd",
-  version         = "1.0",
+  version         = "1.0,0",
   description     = "Yet Another Finite-Difference Time-Domain Framework",
   author          = "shelling",
   author_email    = "navyblueshellingford@gmail.com",
