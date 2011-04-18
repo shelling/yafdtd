8a824d85edcf8240e18f1100bee9107c3d401b09
shelling <navyblueshellingford@gmail.com>
2011-04-18 20:54:31 +0800

    replace string with plane.yinc

--- func-proto/twod-tfsf-tmz-yinc.py
+++ func-proto/twod-tfsf-tmz-yinc.py
@@ -10,44 +10,53 @@ from yafdtd.grid import String, Plane, PBCPlane, UPMLPlane, PlaneDecorator
 from yafdtd.utils import *
 from scipy.constants import c, epsilon_0, mu_0
 
+class TFSFPlane(PlaneDecorator):
+    def __init__(self, orig):
+        super(TFSFPlane, self).__init__(orig)
+        self.xinc = String(self.shape[0])
+        self.yinc = String(self.shape[1])
+        return None
 
 length = 30
 
-string = String(length)
-string.enter = 2
 plane = Plane((length,length))
+
 plane = PBCPlane(plane)
 plane.pbcy = False
+
 plane = UPMLPlane(plane)
 plane.pmlx = False
 plane.set_pml()
 
+plane = TFSFPlane(plane)
+plane.yinc.enter = 2
+
 
 for t in range(0,100):
     print t
-    string.update(sin(2*pi*0.01*t))
-    print string.inspect()
+    plane.yinc.update(sin(2*pi*0.01*t))
+    print plane.yinc.inspect()
 
     plane.update_hpbc()
     plane.update_dfield()
 
-    plane.dzfield[10:21,10] += 0.5 * string.hfield[10]
-    plane.dzfield[10:21,20] -= 0.5 * string.hfield[20]
+    plane.dzfield[10:21,10] += 0.5 * plane.yinc.hfield[10]
+    plane.dzfield[10:21,20] -= 0.5 * plane.yinc.hfield[20]
 
     plane.update_efield()
     plane.update_epbc()
     plane.update_bfield()
 
     # y edge
-    plane.bxfield[10:21,9] += 0.5 * string.dfield[10]
-    plane.bxfield[10:21,20] -= 0.5 * string.dfield[20]
+    plane.bxfield[10:21,9] += 0.5 * plane.yinc.dfield[10]
+    plane.bxfield[10:21,20] -= 0.5 * plane.yinc.dfield[20]
     # x edge
-    plane.byfield[9,10:21] -= 0.5 * string.dfield[10:21]
-    plane.byfield[20,10:21] += 0.5 * string.dfield[10:21]
+    plane.byfield[9,10:21] -= 0.5 * plane.yinc.dfield[10:21]
+    plane.byfield[20,10:21] += 0.5 * plane.yinc.dfield[10:21]
 
     plane.update_hfield()
     
-    plot(string.efield, "/tmp/string-%.3d.png" , t)
+    plot(plane.yinc.efield, "/tmp/string-%.3d.png" , t)
     imshow(plane.ezfield, "/tmp/%.3d.png", t)
 
 print plane.inspect()
