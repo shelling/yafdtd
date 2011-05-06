844115318f7352ce8e6e40ada69f71cc83a64ef1
shelling <navyblueshellingford@gmail.com>
2011-05-06 22:25:03 +0800

    mv twod-test into func-proto

--- func-proto/twod-test.py
+++ func-proto/twod-test.py
@@ -2,6 +2,7 @@
 #-*- mode: python -*-
 
 import sys, os, math, h5py, numpy
+sys.path.append(".")
 
 from yafdtd.source import HardSource, TFSF, sin_oft
 from yafdtd.grid import Plane, PBCPlane, UPMLPlane
