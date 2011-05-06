96689fcca60ba49266464b22f9e2e75799e51c05
shelling <navyblueshellingford@gmail.com>
2011-05-06 22:04:22 +0800

    find the way to perform fft/fftshift on 2D timeline

---
+++ func-proto/fft-twod-timeline.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+#-*- mode: python -*-
+
+import sys, numpy, scipy, matplotlib, pylab
+sys.path.append(".")
+from numpy.fft import fftshift, fft
+
+freq = 100
+dt = 1./10000
+
+t = numpy.arange(10)*dt
+timedomain = numpy.cos(2*numpy.pi*freq*t)
+
+f = numpy.arange(-5,5)/(dt*10)
+freqdomain = numpy.abs(fftshift(fft(timedomain)))
+
+pylab.subplot(2,1,1)
+pylab.plot(t, timedomain)
+pylab.subplot(2,1,2)
+pylab.plot(f, freqdomain)
+pylab.savefig("/tmp/fft-twod-timeline.png")
+
+####################
+
+t2d = numpy.float64(numpy.mgrid[:10,:2,:2][0])*dt
+
+t2d[:,1,1] = 1
+
+timedomain2d = numpy.cos(2*numpy.pi*freq*t2d)
+
+freqdomain2d = numpy.abs(fftshift(fft(timedomain2d, axis=0), axes=(0,) ))
+
+print freqdomain
+print freqdomain2d[:,0,0] / freqdomain
+print freqdomain2d[:,0,1] / freqdomain
+print freqdomain2d[:,1,0] / freqdomain
+print freqdomain2d[:,1,1] / freqdomain
+print freqdomain2d[5]
+
+
+
+
