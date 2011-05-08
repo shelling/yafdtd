f22000508af6cb5a721ff30aac8b17d249aaf83e
shelling <navyblueshellingford@gmail.com>
2011-05-08 15:01:45 +0800

    more examination on fft 2d

--- func-proto/fft-twod-timeline.py
+++ func-proto/fft-twod-timeline.py
@@ -4,6 +4,7 @@
 import sys, numpy, scipy, matplotlib, pylab
 sys.path.append(".")
 from numpy.fft import fftshift, fft
+from numpy import abs
 
 freq = 100
 dt = 1./10000
@@ -12,7 +13,7 @@ t = numpy.arange(10)*dt
 timedomain = numpy.cos(2*numpy.pi*freq*t)
 
 f = numpy.arange(-5,5)/(dt*10)
-freqdomain = numpy.abs(fftshift(fft(timedomain)))
+freqdomain = abs(fftshift(fft(timedomain)))
 
 pylab.subplot(2,1,1)
 pylab.plot(t, timedomain)
@@ -24,18 +25,26 @@ pylab.savefig("/tmp/fft-twod-timeline.png")
 
 t2d = numpy.float64(numpy.mgrid[:10,:2,:2][0])*dt
 
-t2d[:,1,1] = 1
-
 timedomain2d = numpy.cos(2*numpy.pi*freq*t2d)
+timedomain2d[:,0,1] = 1
+timedomain2d[:,1,0] = 2
+timedomain2d[:,1,1] = 3
 
-freqdomain2d = numpy.abs(fftshift(fft(timedomain2d, axis=0), axes=(0,) ))
+freqdomain2d = fftshift(abs(fft(timedomain2d, axis=0)), axes=(0,))
 
 print freqdomain
-print freqdomain2d[:,0,0] / freqdomain
-print freqdomain2d[:,0,1] / freqdomain
-print freqdomain2d[:,1,0] / freqdomain
-print freqdomain2d[:,1,1] / freqdomain
-print freqdomain2d[5]
+print freqdomain2d[:,0,0]/freqdomain
+
+print freqdomain2d
+
+pylab.clf()
+pylab.subplot(2,1,1)
+pylab.plot(t2d[:,0,0], timedomain2d[:,0,0])
+pylab.subplot(2,1,2)
+pylab.plot(f, freqdomain2d[:,0,0])
+pylab.savefig("/tmp/fft2d.png")
+
+
 
 
 
