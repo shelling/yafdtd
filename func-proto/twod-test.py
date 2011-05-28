25093a03534d1ed18cf2ef6296ca520de739432c
shelling <navyblueshellingford@gmail.com>
2011-05-28 16:09:03 +0800

    new 2D prototype

--- func-proto/twod-test.py
+++ func-proto/twod-test.py
@@ -4,10 +4,10 @@
 import sys, os, math, h5py, numpy
 sys.path.append(".")
 
-from yafdtd.source import HardSource, TFSF, sin_oft
 from yafdtd.grid import Plane, PBCPlane, UPMLPlane
 from yafdtd.utils import *
 from scipy.constants import c, epsilon_0, mu_0
+from math import sin, pi
 
 
 name = "twod-test"
@@ -18,32 +18,32 @@ hdf5 = h5py.File("%s/%s.hdf5" % (outdir, name), "w")
 hdf5.attrs["name"] = name 
 hdf5.require_group("timeline")
 
-length = 61
+length = 301
+deltax = 10**-9
+deltat = deltax/(2*c)
+freq   = 4*10**15
 
-dx = 0.01
-dt = dx/c
+plane = UPMLPlane(PBCPlane(Plane((length,length))))
 
-plane = Plane((length,length))
-plane = PBCPlane(plane)
 plane.pbcx = False
-
-plane = UPMLPlane(plane)
-plane.pmly = False
+plane.pbcy = False
+# plane.pmly = False
+plane.pml_thick = 20
 plane.set_pml()
 
 
-for t in range(0,300):
+for t in range(0,600):
     plane.update_hpbc()
     plane.update_dfield()
     plane.update_efield()
 
-    plane.ezfield[30,50] = sin_oft(0.005*t)
+    plane.ezfield[151,151] = sin(2*pi*freq*t*deltat)
 
     plane.update_epbc()
     plane.update_bfield()
     plane.update_hfield()
     
-    plane.hzfield[30,30] = sin_oft(0.005*t)
+    plane.hzfield[151,151] = sin(2*pi*freq*t*deltat)
 
     hdf5.require_group("timeline/"+str(t))
     hdf5["timeline"][str(t)]["ez"] = plane.ezfield
