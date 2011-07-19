8234d7e3c357d1347999df898865ff39b166b9b6
shelling <navyblueshellingford@gmail.com>
2011-07-19 14:04:51 +0800

    cos gaussian with fft

--- func-proto/cos-gaussian.py
+++ func-proto/cos-gaussian.py
@@ -5,28 +5,40 @@ import sys, os, numpy, pylab
 sys.path.append(".")
 
 from yafdtd.source import *
+from yafdtd.utils import fft
 from math import cos, sin, pi
 
 c = 3*10**8
 
-center = 100
+center = 5000
+width = 600
+
 dx = 10**-9
 dt = dx/(2*c)
-freq = 3*10**16
+freq = c/200e-9
 
-f = dt * freq
-print f
+print freq
 
 
-time = numpy.arange(201)
-g = [cos(2*pi*f*t) for t in time]
+pylab.subplot(2,1,1)
+
+time = numpy.arange(20001)
+g = [cos(2*pi*freq*t*dt) for t in time]
 pylab.plot(time, g)
 
-g = [gaussian(t, center, 30) for t in time]
+g = [gaussian(t*dt, center*dt, width*dt) for t in time] # timing dt isn't necessary due to perfect scaling
 pylab.plot(time, g)
 
-g = [cos(2*pi*f*t)*gaussian(t, center, 30) for t in time]
+g = [cos(2*pi*freq*t*dt)*gaussian(t*dt, center*dt, width*dt) for t in time]
 pylab.plot(time, g)
+
+
+
+pylab.subplot(2,1,2)
+
+freq, spectrum = fft(time, g, dt)
+pylab.plot(freq, spectrum)
+pylab.xlim(0,4e15)
 
 pylab.show()
 
